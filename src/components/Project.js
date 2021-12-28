89629201485556bdf41e3049e424ca9b0997c142
devinrbopp <devinrbopp@gmail.com>
2021-12-28 18:20:52 -0500

    added font and cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,9 @@
 export default function Project(props) {
     return(
         <div className='project-card'>
-            <h4>Project Title</h4>
-            <p className='project-tech-stack'>Tech Stack info goes here</p>
-            <p className='project-description'>Here is a brief description of the project.</p>
+            <h4>{props.title}</h4>
+            <p className='project-tech-stack'>{props.techStack}</p>
+            <p className='project-description'>{props.description}</p>
         </div>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,10 +2,25 @@ import Project from "./Project"
 
 export default function Projects() {
     return(
-        <div id='projects'>
+        <>
             <h3>Take a look at my personal projects:</h3>
-            <Project />
-            <Project />
-        </div>
+            <div id='projects'>
+                <Project
+                    title="it's getting colder"
+                    techStack='HTML, CSS, JavaScript'
+                    description='Enim et in esse duis duis. Est anim non id mollit voluptate enim Lorem aliqua aliquip. Nostrud sint labore aliquip enim aliqua.'
+                />
+                <Project
+                    title="Present-ly Shopping Planner"
+                    techStack='PostgreSQL, Express, Node'
+                    description='Nisi qui esse non fugiat non cillum amet fugiat esse sint et. Ipsum sint ad occaecat tempor in. Aute culpa excepteur sint minim amet dolore. Consequat reprehenderit quis est aliquip nostrud sit consequat ullamco amet sunt.'
+                />
+                <Project
+                    title="MinMax Job Hunter"
+                    techStack='MongoDB, Express, React, Node'
+                    description='Lorem labore culpa reprehenderit magna fugiat reprehenderit reprehenderit reprehenderit eiusmod cillum Lorem nulla. Ad nisi deserunt deserunt nostrud pariatur id laboris ea commodo nisi. Amet enim sunt officia id adipisicing veniam nisi aliquip labore reprehenderit.'
+                />        
+            </div>
+        </>
     )
 }
