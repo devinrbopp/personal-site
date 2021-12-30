a5de7efe30de91f739ad8a8ebf6264f04bab73aa
devinrbopp <devinrbopp@gmail.com>
2021-12-29 19:20:58 -0500

    added deploy links

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,6 +4,7 @@ export default function Project(props) {
             <h4>{props.title}</h4>
             <p className='project-tech-stack'>{props.techStack}</p>
             <p className='project-description'>{props.description}</p>
+            <a className='deploy-link' href={props.url} target='_blank'>Deployed App</a>
         </div>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,16 +8,19 @@ export default function Projects() {
                 <Project
                     title="it's getting colder"
                     techStack='HTML, CSS, JavaScript'
+                    url='https://devinrbopp.github.io/its-getting-colder-game/'
                     description='Enim et in esse duis duis. Est anim non id mollit voluptate enim Lorem aliqua aliquip. Nostrud sint labore aliquip enim aliqua.'
                 />
                 <Project
                     title="Present-ly Shopping Planner"
                     techStack='PostgreSQL, Express, Node'
+                    url='https://present-ly-gift-planner.herokuapp.com/'
                     description='Nisi qui esse non fugiat non cillum amet fugiat esse sint et. Ipsum sint ad occaecat tempor in. Aute culpa excepteur sint minim amet dolore. Consequat reprehenderit quis est aliquip nostrud sit consequat ullamco amet sunt.'
                 />
                 <Project
                     title="MinMax Job Hunter"
                     techStack='MongoDB, Express, React, Node'
+                    url='https://minmax-job-hunter.herokuapp.com/'
                     description='Lorem labore culpa reprehenderit magna fugiat reprehenderit reprehenderit reprehenderit eiusmod cillum Lorem nulla. Ad nisi deserunt deserunt nostrud pariatur id laboris ea commodo nisi. Amet enim sunt officia id adipisicing veniam nisi aliquip labore reprehenderit.'
                 />        
             </div>
