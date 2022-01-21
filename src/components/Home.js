a226e1417610e6a25b890d671ac0121a9cfc38a8
devinrbopp <devinrbopp@gmail.com>
2022-01-21 15:50:32 -0700

    added blocks for tech stack

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import Projects from "./Projects"
 import Contact from "./Contact"
+import TechStack from "./TechStack"
 
 export default function Home(props) {
     return(
@@ -7,6 +8,7 @@ export default function Home(props) {
             <h1>hello, i'm devin!</h1>
             <p>I am a full-stack software engineer with a past life in nonprofit fundraising. With experience in fast-paced, passionate workplaces, I thrive on creative, collaborative teams working to maximize resources with inventive software solutions. A passionate autodidact, I am eager to further my knowledge across programming stacks to better serve my colleagues, my clients, and my own vision.</p>
             <Projects />
+            <TechStack />
             <Contact />
         </div>
     )
---
+++ src/components/TechStack.js
@@ -0,0 +1,15 @@
+export default function TechStack(props) {
+    return (
+        <div className="tech-stack">
+            <div className='shadow tech-stack-card' id='languages'>
+                <p>languages</p>
+            </div>
+            <div className='shadow tech-stack-card' id='frameworks-databases'>
+                <p>frameworks and databases</p>
+            </div>
+            <div className='shadow tech-stack-card' id='other-skills'>
+                <p>other skills</p>
+            </div>
+        </div>
+    )
+}
