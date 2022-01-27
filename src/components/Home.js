f02117668b11ebca643541637cd043248c802f1b
devinrbopp <devinrbopp@gmail.com>
2022-01-27 16:39:32 -0700

    prepared heroku deploy

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ export default function Home(props) {
     return(
         <div className="page-content">
             <h1>hello, i'm devin!</h1>
-            <p>I am a full-stack software engineer with a past life in nonprofit fundraising. With experience in fast-paced, passionate workplaces, I thrive on creative, collaborative teams working to maximize resources with inventive software solutions. A passionate autodidact, I am eager to further my knowledge across programming stacks to better serve my colleagues, my clients, and my own vision.</p>
+            <p>I am a <b>full-stack software engineer</b> with a past life in nonprofit fundraising. I thrive on creative, collaborative teams working to maximize resources with inventive software solutions.</p>
             <Projects />
             <TechStack />
             <Contact />
